Fixing minor bugs and computing softmax along all the similarities

src/extract_features/extract.py
@@ -19,12 +19,11 @@ model, preprocess = clip.load("ViT-B/32", device=device)
 
 
 @timing
-def encode_search_query(search_query: list):
+def encode_search_query(search_query: str):
     with torch.no_grad():
         # Encode and normalize the search query using CLIP
         text_encoded = model.encode_text(clip.tokenize(search_query).to(device))
         text_encoded /= text_encoded.norm(dim=-1, keepdim=True)
-    # Retrieve the feature vector
     return text_encoded.cpu().numpy()
 
 
@@ -33,22 +32,23 @@ def encode_image_batch(photos_batch: list):
     photos = [Image.open(photo_file) for photo_file in photos_batch]
     # Preprocess all photos
     photos_preprocessed = torch.stack([preprocess(photo) for photo in photos]).to(device)
-
     with torch.no_grad():
         # Encode the photos batch to compute the feature vectors and normalize them
         photos_features = model.encode_image(photos_preprocessed)
-        photos_features /= photos_features.norm(dim=-1, keepdim=True)
-
-    # Transfer the feature vectors back to the CPU and convert to numpy
+    photos_features /= photos_features.norm(dim=-1, keepdim=True)
     return photos_features.cpu().numpy()
 
 
 @timing
-def extract_new_images(images_path, photo_ids):
+def extract_new_images(images_path: str, photo_ids: str):
     with open(photo_ids) as f:
         photos = f.readlines()
+    photos = [photo.rstrip() for photo in photos]
     list_of_images = get_images(images_path)
     new_list = list(set(list_of_images) - set(photos))
+    if not new_list:
+        log.info('No new images were found')
+        return
     log.info(f'New {len(new_list)} images found. Starting to extract')
     encode_all_images(new_list, 8, photo_ids)
 
@@ -70,23 +70,24 @@ def encode_all_images(list_of_images: list, batch_size: int, photo_ids: str):
             batch = list_of_images[i:i+batch_size]
             batch_features = encode_image_batch(batch)
             progress_bar.update(batch_size)
-            np.save(f'features/batch_{i}_{i + batch_size}.npy', batch_features)
-            photos = [filename for filename in batch]
-            photo_ids_data = pd.DataFrame(photos, columns=['photo_id'])
-            photo_ids_data.to_csv(f'features/batch_{i}_{i + batch_size}.csv', index=False)
+            np.save(f'features/batch_{i}.npy', batch_features)
+            photo_ids_data = pd.DataFrame(batch)
+            photo_ids_data.to_csv(f'features/batch_{i}.csv', index=False)
 
     log.info('Features extracted')
-    csv_files = sorted(glob.glob("features/*.csv"))
+    csv_files = sorted(glob.glob("features/batch_*.csv"))
     photos_df = pd.concat([pd.read_csv(ids_file) for ids_file in csv_files])
+
     if os.path.exists(photo_ids):
         log.info('Merging previous photo_ids with the latest update')
         previous_photo_ids = pd.read_csv(photo_ids)
-        photo_df = pd.concat([previous_photo_ids, photos_df])
-    photos_df.to_csv(photo_ids, index=False)
+        photos_df = pd.concat([previous_photo_ids, photos_df])
+    photos_df.to_csv(photo_ids, index=False, header=False)
 
     log.info('Merging batch features')
     batch_files = sorted(glob.glob('features/*.npy'))
     features = np.concatenate([np.load(features_file) for features_file in batch_files])
+
     log.info(f'Features computed of shape {features.shape}')
     if os.path.exists('features/features.h5'):
         log.info('Merging previous features with the latest update')

src/search/search.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import pickle
 
 import h5py
 import numpy as np
@@ -11,14 +10,15 @@ from src.extract_features.extract import encode_search_query, extract_new_images
 from src.util.time_util import timing
 
 
-@timing
-def find_best_matches(text_features: npt.ArrayLike, image_features: npt.ArrayLike):
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+def compute_similarity(text_features: npt.ArrayLike, image_features: npt.ArrayLike):
     # Compute the similarity between the search query and each photo using the Cosine similarity
-    similarities = (image_features @ text_features.T).squeeze(1)
-    # Sort the photos by their similarity score
-    # TODO use a threshold to approach empty results
-    best_photo_idx = (-similarities).argsort()
-    return best_photo_idx, similarities
+    similarities = 100*(text_features @ image_features.T)
+    return softmax(similarities[0])
 
 
 @timing
@@ -26,10 +26,10 @@ def search(search_query: str, photo_features: npt.ArrayLike, photo_ids: list, re
     # Encode the search query
     text_features = encode_search_query(search_query)
     # Find the best matches
-    best_photos, similarities = find_best_matches(text_features, photo_features)
-    result = [(photo_ids[i], similarities[i]) for i in best_photos[:results_count]]
-    log.info(f'Result:{result}')
-    return result
+    similarities = compute_similarity(text_features, photo_features)
+    best_match = sorted(zip(similarities, range(len(photo_features))), key=lambda x: x[0], reverse=True) # TODO argsort
+    for i in range(results_count):
+        log.info(f'{i}: {photo_ids[best_match[i][1]].rstrip()} with a similarity of {round(100*best_match[i][0],3)}%')
 
 
 if __name__ == '__main__':
@@ -49,7 +49,7 @@ if __name__ == '__main__':
         else:
             raise Exception("You need to specify the photos path if the features do not exist")
     elif args.images:
-        log.info('Extracting new images inserted to the database')
+        log.info('Checking for new images inserted to the database')
         extract_new_images(args.images, args.photo_ids)
 
     with open(args.photo_ids) as f:
@@ -58,5 +58,6 @@ if __name__ == '__main__':
     with h5py.File(args.features, 'r') as hf:
         photo_features = hf['features'][:]
 
+    assert args.results_count <= len(photo_ids)
     log.info(f'Searching for {args.query} in our image database of {len(photo_ids)} images')
     search(args.query, photo_features, photo_ids, args.results_count)